d8c11f89c8a002c0c2bd89de463c765dbf54f5e2
chengahtung <ttmagicmage@hotmail.com>
2019-06-16 17:35:45 +0800

    version 2

--- app/src/main/java/com/assignment/restaurantreservation/LandingActivity.java
+++ app/src/main/java/com/assignment/restaurantreservation/LandingActivity.java
@@ -58,6 +58,8 @@ public class LandingActivity extends AppCompatActivity {
             }
         });
 
+
+
     }
 
 
--- app/src/main/java/com/assignment/restaurantreservation/ReservationFragment.java
+++ app/src/main/java/com/assignment/restaurantreservation/ReservationFragment.java
@@ -9,15 +9,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TimePicker;
+
 
 import java.util.Calendar;
 
 
 public class ReservationFragment extends Fragment implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
+    String Noofseat[] = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"};
+    String Prefseatdropdown[] = {" ","Corner","Near bathroom","View","Sofa"};
+    String hehe = "hest";
+    ArrayAdapter<String>myadapter;
     EditText datepick, timepick;
     int day, month, year, hour, minute;
     int day_x, month_x, year_x, hour_x, minute_x;
@@ -26,6 +33,8 @@ public class ReservationFragment extends Fragment implements DatePickerDialog.On
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_reservation, null);
+
+
 
         datepick = view.findViewById(R.id.DatePick);
         datepick.setOnClickListener(new View.OnClickListener() {
@@ -58,6 +67,19 @@ public class ReservationFragment extends Fragment implements DatePickerDialog.On
 
             }
         });
+
+        //Number seat drop down
+        Spinner spinner1  = view.findViewById(R.id.Spinner1);
+        myadapter=new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,Noofseat);
+        myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(myadapter);
+
+
+        //prefered seat drop down
+        Spinner spinner2  = view.findViewById(R.id.Spinner2);
+        myadapter=new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,Prefseatdropdown);
+        myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(myadapter);
 
         return view;
 
